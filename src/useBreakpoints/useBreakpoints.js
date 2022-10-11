9fb03dc2e96a3c720fa491a17e0603566e2c0ed8
carpenjk <56424605+carpenjk@users.noreply.github.com>
2022-10-11 19:33:01 -0400

    feat(usebreakpoints.js): now uses breakpoints directly as a parameter instead of entire theme
    BREAKING CHANGE: change useBreakpoint function call from useBreakpoints(theme) to
    useBreakpoints(breakpoints)

--- src/useBreakpoints/useBreakpoints.js
+++ src/useBreakpoints/useBreakpoints.js
@@ -2,32 +2,32 @@ import useWindowSize from '../useWindowSize'
 import { useEffect, useState } from 'react'
 import { getBreakpointPixels, getIndexOfLower, getLower, getRatio, getUpper, toArray } from './breakpoints'
 
-export const useBreakpoints = (theme) => {
+export const useBreakpoints = (brValues) => {
   const windowSize = useWindowSize()
 
   const [breakpoints, setBreakpoints] = useState({
-    br: getBreakpointPixels(theme.breakpoints),
-    breakpoints: theme.breakpoints,
-    upper: getUpper(theme.breakpoints),
-    lower: getLower(theme.breakpoints),
-    indexOfLower: getIndexOfLower(theme.breakpoints),
-    ratio: () => getRatio(theme.breakpoints, windowSize), // in consideration for future intellegent br functionality
+    br: getBreakpointPixels(brValues),
+    breakpoints: brValues,
+    upper: getUpper(brValues),
+    lower: getLower(brValues),
+    indexOfLower: getIndexOfLower(brValues),
+    ratio: () => getRatio(brValues, windowSize), // in consideration for future intellegent br functionality
     current: { width: windowSize.width, height: windowSize.height },
-    toArray: toArray(theme.breakpoints)
+    toArray: toArray(brValues)
   })
 
   useEffect(() => {
     setBreakpoints({
-      br: getBreakpointPixels(theme.breakpoints),
-      breakpoints: theme.breakpoints,
-      upper: getUpper(theme.breakpoints),
-      lower: getLower(theme.breakpoints),
-      indexOfLower: getIndexOfLower(theme.breakpoints),
-      ratio: getRatio(theme.breakpoints, windowSize), // in consideration for future intellegent br functionality
+      br: getBreakpointPixels(brValues),
+      breakpoints: brValues,
+      upper: getUpper(brValues),
+      lower: getLower(brValues),
+      indexOfLower: getIndexOfLower(brValues),
+      ratio: getRatio(brValues, windowSize), // in consideration for future intellegent br functionality
       current: windowSize,
-      toArray: toArray(theme.breakpoints)
+      toArray: () => toArray(brValues)
     })
-  }, [windowSize, theme.breakpoints])
+  }, [windowSize, brValues])
   return breakpoints
 }
 

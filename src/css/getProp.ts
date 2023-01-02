8ae2c6d41a9b0c7473e4c5a5dd6abb8388b458f8
carpenjk <56424605+carpenjk@users.noreply.github.com>
2023-01-02 12:18:37 -0500

    fix(getprop.ts): changed type import syntax to import type
    changed type import syntax to import type to fix import errors in build

--- src/css/getProp.ts
+++ src/css/getProp.ts
@@ -1,7 +1,7 @@
 import { getIndexedPropValue } from '../core'
 import { deriveIndex } from '../internal';
-import { NestedCSSFunction } from '../../types/functionTypes'
-import { BreakpointIndex, BreakpointProps } from '../../types/propTypes'
+import type { NestedCSSFunction } from '../../types/functionTypes'
+import type { BreakpointIndex, BreakpointProps } from '../../types/propTypes'
 // returns responsive prop value within styled component tag function
 export const getProp = (prop: string): NestedCSSFunction => (props: BreakpointProps, br?: BreakpointIndex): string => {
   const derivedBr = deriveIndex(br);
